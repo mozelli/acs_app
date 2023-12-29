03d9d7e60e803381ca0b4c9227defc73dabfd16e
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 18:38:48 -0300

    configure express

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from "dotenv";
+import { routes } from './routes';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+
+app.listen(process.env.PORT, () => console.log(`APP Server is running -> localhost:${process.env.PORT}`));
