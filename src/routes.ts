fd94fc0f6c00aa48bdd3543037cf10f2106c3572
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 20:36:19 -0300

    create createPost route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import express, {Request, Response} from 'express';
+import PostController from './controllers/PostsController';
 
 const routes = express.Router();
+const postController = new PostController();
 
 routes.get('/', (req: Request, res: Response) => {
     return res.json({Hello: "World!"});
 });
+
+routes.post('/posts', postController.createPost);
 
 export { routes };
