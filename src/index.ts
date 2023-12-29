90f9a39fe98a5666657a3f378fa2acb00fde27d2
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 20:34:36 -0300

    allow connection

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import { routes } from './routes';
+import { connect } from './database';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+connect();
+
 app.use(routes);
 
 app.listen(process.env.PORT, () => console.log(`APP Server is running -> localhost:${process.env.PORT}`));
